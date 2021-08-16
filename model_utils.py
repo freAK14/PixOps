9d7036aed3d9c1d5e74d3803e8084b5b4746d4bf
Akash Kothare <akashsocial14@gmail.com>
2021-08-17 02:37:31 +0530

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -4,3 +4,8 @@ Created on Tue Aug 17 02:27:54 2021
 
 @author: Akash
 """
+import numpy as np
+import tensorflow as tf
+
+def pixel_shuffle(scale):
+    return lambda x: tf.nn.depth_to_space(x, scale)
