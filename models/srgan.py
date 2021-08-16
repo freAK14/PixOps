9d99e8f16854736c1461ced1ebd691c8cc732b64
Akash Kothare <akashsocial14@gmail.com>
2021-08-17 02:37:34 +0530

    Update srgan.py

--- models/srgan.py
+++ models/srgan.py
@@ -8,6 +8,13 @@ from tensorflow.python.keras.layers import Add, BatchNormalization, Conv2D, Dens
 from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.applications.vgg19 import VGG19
 
+from model_utils import pixel_shuffle
 
 LR_SIZE = 24
 HR_SIZE = 96
+
+def upsample(x_in, num_filters):
+    x = Conv2D(num_filters, kernel_size = 3, padding = 'same')(x_in)
+    x = Lambda(pixel_shuffle(scale = 2))(x)
+    return PReLU(shared_axes=[1, 2])(x)
+
