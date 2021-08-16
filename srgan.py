dac2b16e765d2b541e3e8a57f25c9dc4872d278e
Akash Kothare <akashsocial14@gmail.com>
2021-08-17 02:27:49 +0530

    Update srgan.py

--- srgan.py
+++ srgan.py
@@ -4,4 +4,10 @@ Created on Tue Aug 17 02:25:10 2021
 
 @author: Akash
 """
+from tensorflow.python.keras.layers import Add, BatchNormalization, Conv2D, Dense, Flatten, Input, LeakyReLU, PReLU, Lambda
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.applications.vgg19 import VGG19
 
+
+LR_SIZE = 24
+HR_SIZE = 96
