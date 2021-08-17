53f335b82fd1cca93963860133d8910a8f2802f6
Akash Kothare <akashsocial14@gmail.com>
2021-08-18 01:45:59 +0530

    Update srgan.py

--- models/srgan.py
+++ models/srgan.py
@@ -81,3 +81,12 @@ def discriminator(num_filters=64):
     
     return Model(x_in, x)
 
+def vgg_22():
+    return _vgg(5)
+
+def vgg_54():
+    return _vgg(20)
+
+def _vgg(output_layer):
+    vgg = VGG19(input_shape=(None, None, 3), include_top=False)
+    return Model(vgg.input, vgg.layers[output_layer].output)
