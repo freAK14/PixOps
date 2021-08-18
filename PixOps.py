d15b2622655ab6b9ba538b60fddcfff0a270d186
Akash Kothare <akashsocial14@gmail.com>
2021-08-19 03:29:43 +0530

    Update PixOps.py

--- PixOps.py
+++ PixOps.py
@@ -5,21 +5,27 @@ Created on Thu Aug 19 01:44:43 2021
 @author: Akash
 """
 import os
+import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from PIL import Image
 
 class super_resolution:
     def __init__(self):
-        from models.srgan import generator
-        from models.model_utils import resolve_single
-        from utils import load_image
-        weights_dir = "weights/srgan"
-        weights_file = lambda filename: os.path.join(weights_dir, filename)
+        pass
         
     def gen_sr(self, image_path):
-        gan_generator = self.generator()
-        gan_generator.load_weights(self.weights_file('gan_generator.h5'))
+        from models.model_utils import resolve_single
+        from models.srgan import generator
+        weights_dir = "weights/srgan"
+        weights_file = lambda filename: os.path.join(weights_dir, filename)
+        gan_generator = generator()
+        gan_generator.load_weights(weights_file('gan_generator.h5'))
         
-        img = self.load_image(image_path)
-        sr = self.resolve_single(gan_generator, img)
+        img = np.array(Image.open(image_path))
+        sr = resolve_single(gan_generator, img)
         
-        plt.imshow(sr)
+        #plt.imshow(sr)
+        #plt.savefig("./demo_images/op.png")
+        pil_img = tf.keras.preprocessing.image.array_to_img(sr)
+        pil_img.save('./demo_images/op.png')
